8611781ca04998f1ddf255e0807527742bc49173
Hanna-Maryia Savitskaya <103072354+sans-et@users.noreply.github.com>
2024-04-09 12:23:25 +0300

    main page

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import logo from "./logo.svg";
+import parser from 'bbcode-to-react';
+import racoon from "./Raccoon.png"
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   <div className="App">
+      <header className="App-header"> 
+      <img src={racoon} alt="Raccoon logo" className="RaccconLogo"></img>
+       <div className="Project">LEGACY</div>
       </header>
+
+      <body>
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code></code> 
+        </p>
+      </body>
+       
+      <footer className="App-footer">
+        <a className="FooterText">
+        ⓒ IT-FAMCS 2024
+        </a>
+      </footer>
     </div>
   );
 }
