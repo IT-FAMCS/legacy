362079f33050ca0a657f1ce4f551cfa56ba35d01
Evgeniy Shevtsov <yevgeniy.shevtsov.pblcb@mail.ru>
2024-04-26 17:21:37 +0300

    fix: footer

--- front-app/src/components/Footer.tsx
+++ front-app/src/components/Footer.tsx
@@ -1,9 +1,17 @@
-export default function Footer() {
+import { Button } from "@mui/material";
 
+export default function Footer() {
   return (
     <footer className="App-footer">
-        <div className="FooterText">ⓒ IT-FAMCS 2024</div>
+      <div className="FooterText">ⓒ IT-FAMCS 2024</div>
+      <Button
+        className="CreatorButton"
+        href="/creators"
+        variant="outlined"
+        sx={{ color: "white", borderColor: "white" }}
+      >
+        О создателях
+      </Button>
     </footer>
-  )
+  );
 }
-
