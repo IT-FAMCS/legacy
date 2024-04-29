e7f676c14b6a8220314dc75878ff96ad0e026759
Kseniya <pupkoksenia@gmail.com>
2024-04-30 00:34:06 +0300

    LGC-17 add header button, refactoring css names in headerBEM metology

--- front-app/src/components/Header.tsx
+++ front-app/src/components/Header.tsx
@@ -3,6 +3,7 @@ import racoon from "../images/Raccoon.png";
 import IconButton from "@mui/material/IconButton";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { Button } from "@mui/material";
 
 interface HeaderProps {
   themeMode: string;
@@ -12,13 +13,31 @@ interface HeaderProps {
 export const HeaderComponent = ({ toggleTheme, themeMode }: HeaderProps) => {
   return (
     <header className="App-header">
-      <div className="HeaderLeft">
+      <div className="App-header__block">
         <img src={racoon} alt="Raccoon logo" className="RaccconLogo"></img>
         <div className="Project">LEGACY</div>
       </div>
+      <div className="App-header__block">
+       <Button
+        href={"/"}
+        variant="contained"
+          sx={{
+          width: 150,
+          color: "white",
+          height: 40,
+          margin: 5,
+        }}
+      >
+        <div className="ButtonText">
+          <h2>Home</h2>
+        </div>
+      </Button>
       <IconButton sx={{ ml: 1 }} onClick={toggleTheme} color="inherit">
         {themeMode === "dark" ? <Brightness4Icon /> : <Brightness7Icon />}
       </IconButton>
+      </div>
+      
+      
     </header>
   );
 };
