997bb0c3febed2acaad5e6a4088453077841b693
Artyom Korobkin <carlionminon@gmail.com>
2024-05-07 18:06:30 +0300

    upd: rewrite old models

--- department/serializer.py
+++ department/serializer.py
@@ -8,22 +8,27 @@ class LinksSerializer(serializers.ModelSerializer):
         
     
 class DepartmentSerializer(serializers.ModelSerializer):
-    links = LinksSerializer(many=True)  
-    
+    links = LinksSerializer(many=True)
+
     class Meta:
         model = Department
         fields = '__all__'
-    
+
     def create(self, validated_data):
-        
-        links_data = validated_data.pop('links', None)
-        
-        
-        department = Department.objects.create(**validated_data)
-        
-        
-        if links_data:
+        links_data = validated_data.pop('links', [])
+        depart_name = validated_data.get('short_name')
+
+        try:
+            department = Department.objects.get(short_name=depart_name)
+            for key, value in validated_data.items():
+                setattr(department, key, value)
+            department.save()
+
+            for link_data in links_data:
+                link, created = Links.objects.update_or_create(department=department, **link_data)
+        except Department.DoesNotExist:
+            department = Department.objects.create(**validated_data)
             for link_data in links_data:
                 Links.objects.create(department=department, **link_data)
-        
+                
         return department
--- information/models.py
+++ information/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Information(models.Model):
-    short_title = models.CharField(max_length = 10)
+    short_title = models.CharField(max_length = 10, unique=True)
     boss_info = models.TextField()
     advises = models.TextField()
     how_to = models.TextField()
