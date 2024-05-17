360a76a4e73354786ff667df0ec3f2fb63aa5cd5
Hanna-Maryia Savitskaya <103072354+sans-et@users.noreply.github.com>
2024-05-17 22:10:45 +0300

    add: hooks

---
+++ front-app/src/Pages/Forms/EventForm.tsx
@@ -0,0 +1,11 @@
+export function EventForm({}) {
+  return (
+    <form>
+      <input type="text" name="title" />
+      <textarea name="description" />
+      <textarea name="work" />
+      <textarea name="FAQ" />
+      <textarea name="links" />
+    </form>
+  );
+}
