70ab7acdaf3f955c5d11902520e023cfe65bed2d
Anastasia Karpushevich <a.karpushevich.a@gmail.com>
2024-06-02 21:59:48 +0300

    add: hooks to change and get questions

---
+++ front-app/src/Constants/questions-info.ts
@@ -0,0 +1,22 @@
+export const questionsInfo = [
+  {
+    title: "Вопрос 1",
+    content: "Ответ 1",
+  },
+  {
+    title: "Вопрос 2",
+    content: "Ответ 2",
+  },
+  {
+    title: "Вопрос 3",
+    content: "Ответ 3",
+  },
+  {
+    title: "Вопрос 4",
+    content: "Ответ 4",
+  },
+  {
+    title: "Вопрос 5",
+    content: "Ответ 5",
+  }
+];
